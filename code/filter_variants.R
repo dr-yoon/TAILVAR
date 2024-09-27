5b80a8b82b374692a32152cfaf7acbd99f9ff08f
Jihoon G. Yoon <114216377+dr-yoon@users.noreply.github.com>
2024-09-27 12:57:12 +0900

    Update filter_variants.R

--- code/filter_variants.R
+++ code/filter_variants.R
@@ -15,17 +15,16 @@ dataset <- dataset %>% filter(Feature %in% transcript_info$ensembl_transcript_id
                          Amino_acids = str_extract(Amino_acids, "(?<=/).*"), extended_length = str_extract(HGVSp, "(?<=fsTer|extTer)(\\d+|\\?)")) %>%
   separate(Codons, into = c("Codons_REF", "Codons_ALT"), sep = "\\/") %>% separate(Location, into = c("Chromosome", "Position"), sep = ":")
 
-Pop_AF <-c("UK10K_AF", "ESP6500_AA_AF", "ESP6500_EA_AF", "gnomAD_exomes_AF", "gnomAD_exomes_POPMAX_AF", "gnomAD_genomes_AF", "gnomAD_genomes_POPMAX_AF", "ALFA_Total_AF")
-comp_scores <- c("CADD_phred", "DANN_score", "MutationTaster_score", "fathmm-MKL_coding_score", "Eigen-phred_coding", "BayesDel_addAF_score", "BayesDel_noAF_score", "integrated_fitCons_score", "GERP++_RS", "phyloP100way_vertebrate", "phastCons100way_vertebrate")
+Pop_AF <-c("gnomAD_exomes_AF", "gnomAD_exomes_POPMAX_AF", "gnomAD_genomes_AF", "gnomAD_genomes_POPMAX_AF")
+comp_scores <- c("CADD_phred", "DANN_score", "fathmm-MKL_coding_score", "Eigen-phred_coding", "BayesDel_addAF_score", "BayesDel_noAF_score", "integrated_fitCons_score", "GERP++_RS", "phyloP100way_vertebrate", "phastCons100way_vertebrate")
 
 dataset <- dataset %>% 
-  mutate(MutationTaster_score = sub(",.*", "", MutationTaster_score)) %>%
   mutate(across(all_of(Pop_AF), ~as.numeric(.))) %>% mutate(across(all_of(Pop_AF), ~as.numeric(replace_na(., 0)))) %>%
   mutate(across(all_of(comp_scores), ~as.numeric(.)))
 
 dataset <- dataset %>% dplyr::select(-Gene, -Feature_type, -Existing_variation, -IMPACT, -DISTANCE, -FLAGS, -VARIANT_CLASS, -SYMBOL_SOURCE, -HGNC_ID, -BIOTYPE, -MANE_PLUS_CLINICAL, -SOURCE, -INTRON, -HGVS_OFFSET) %>% arrange(Chromosome)
 
-comp_scores_rename <- c("CADD", "DANN", "MutationTaster", "FATHMM", "EIGEN", "BayesDel_addAF", "BayesDel_noAF", "int_fitCons", "GERP", "phyloP100way", "phastCons100way")
+comp_scores_rename <- c("CADD", "DANN", "FATHMM", "Eigen", "BayesDel_addAF", "BayesDel_noAF", "integrated_fitCons", "GERP", "phyloP100way", "phastCons100way")
 
 dataset <- dataset %>% rename_at(vars(comp_scores), ~ comp_scores_rename)
 
