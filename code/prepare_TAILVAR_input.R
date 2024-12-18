9f279580d0800b95144c2f7119283051db153bde
Jihoon G. Yoon <114216377+dr-yoon@users.noreply.github.com>
2024-12-18 15:47:19 +0900

    Update prepare_TAILVAR_input.R

--- code/prepare_TAILVAR_input.R
+++ code/prepare_TAILVAR_input.R
@@ -5,65 +5,67 @@ library(tidyverse)
 transcript_info <- read_tsv("mane_transcripts_info.txt")
 transcript_info <- transcript_info %>% dplyr::select(ensembl_transcript_id, percentage_gene_gc_content, length_3utr, gc_3utr, stop_codons, next_stop_codon, ext_length, translate_tail)
 transcript_info_colname <- c("ensembl_transcript_id", "percentage_gene_gc_content", "length_3utr", "gc_3utr", "ext_length", "translate_tail")
-transcript_info_rename <- c("Feature", "Gene_GC", "UTR3_length", "UTR3_GC", "TailAA_counts", "TailAA_seq")
+transcript_info_rename <- c("Feature", "Gene_GC", "UTR3_length", "UTR3_GC", "Extension_lengths", "Extension_seq")
 
 transcript_info <- transcript_info %>% rename_at(vars(transcript_info_colname), ~ transcript_info_rename)
 
-# Load all possible stoplost SNV data
-stoplost_all <- read_tsv("stoplost_SNV_filtered.txt")
-stoplost_all <- stoplost_all %>% mutate(Class = "")
-stoplost_all <- left_join(stoplost_all, transcript_info, by = c("Feature")) %>% distinct()
-stoplost_all <- stoplost_all %>% mutate(TailAA_seq = paste0(Amino_acids, TailAA_seq))
-stoplost_all <- stoplost_all %>% filter(is.na(TailAA_counts) == FALSE)
+# Load all possible stoploss SNV data
+stoploss_all <- read_tsv("stoploss_SNV_filtered.txt")
+stoploss_all <- stoploss_all %>% mutate(Class = "")
+stoploss_all <- left_join(stoploss_all, transcript_info, by = c("Feature")) %>% distinct()
+stoploss_all <- stoploss_all %>% mutate(Extension_seq = paste0(Amino_acids, Extension_seq))
+stoploss_all <- stoploss_all %>% filter(is.na(Extension_lengths) == FALSE)
 
 # Load Clinvar, HGMD data
-stoplost_clinvar <- read_tsv("stoplost_Clinvar_filtered.txt")
-stoplost_clinvar <- stoplost_clinvar %>% filter(Consequence %in% c("stop_lost")) %>%
+stoploss_clinvar <- read_tsv("stoploss_Clinvar_filtered.txt")
+stoploss_clinvar <- stoploss_clinvar %>% filter(Consequence %in% c("stop_lost")) %>%
   filter(ClinVar_CLNSIG %in% c("Pathogenic", "Likely_pathogenic", "Pathogenic/Likely_pathogenic","Uncertain_significance","Benign", "Likely_benign", "Benign/Likely_benign")) %>%
   mutate(
     Class = case_when(str_detect(ClinVar_CLNSIG, regex("athogenic", ignore_case = TRUE)) ~ "P",
                       str_detect(ClinVar_CLNSIG, regex("ncertain", ignore_case = TRUE)) ~ "VUS", 
                       str_detect(ClinVar_CLNSIG, regex("enign", ignore_case = TRUE)) ~ "B",
+                      str_detect(ClinVar_CLNSIG, regex("Conflicting", ignore_case = TRUE)) ~ "C",
                       TRUE ~ ClinVar_CLNSIG),
   )
-stoplost_clinvar <- left_join(stoplost_clinvar, transcript_info, by = c("Feature")) %>% distinct()
-stoplost_clinvar <- stoplost_clinvar %>% mutate(TailAA_seq = paste0(Amino_acids, TailAA_seq)) %>% filter(is.na(TailAA_counts) == FALSE)
+stoploss_clinvar <- left_join(stoploss_clinvar, transcript_info, by = c("Feature")) %>% distinct()
+stoploss_clinvar <- stoploss_clinvar %>% mutate(Extension_seq = paste0(Amino_acids, Extension_seq)) %>% filter(is.na(Extension_lengths) == FALSE)
+write.table(stoploss_clinvar, "Test_clinvar_input.txt", row.names = FALSE, sep = "\t", quote = FALSE)
 
-stoplost_HGMD <- read_tsv("stoplost_HGMD_filtered.txt")
-stoplost_HGMD <- stoplost_HGMD %>% mutate(Class = "P")
-stoplost_HGMD <- left_join(stoplost_HGMD, transcript_info, by = c("Feature")) %>% distinct()
-stoplost_HGMD <- stoplost_HGMD %>% mutate(TailAA_seq = paste0(Amino_acids, TailAA_seq))
-stoplost_HGMD <- stoplost_HGMD %>% filter(is.na(TailAA_counts) == FALSE)
+stoploss_HGMD <- read_tsv("stoploss_HGMD_filtered.txt")
+stoploss_HGMD <- stoploss_HGMD %>% mutate(Class = "P")
+stoploss_HGMD <- left_join(stoploss_HGMD, transcript_info, by = c("Feature")) %>% distinct()
+stoploss_HGMD <- stoploss_HGMD %>% mutate(Extension_seq = paste0(Amino_acids, Extension_seq))
+stoploss_HGMD <- stoploss_HGMD %>% filter(is.na(Extension_lengths) == FALSE)
 
 # Load Population DB: gnomAD, ALFA > 0.001
-gnomAD_MAF_0.001 <- stoplost_all %>% filter(gnomAD_exomes_POPMAX_AF > 0.001 | gnomAD_genomes_POPMAX_AF > 0.001)
+gnomAD_MAF_0.001 <- stoploss_all %>% filter(gnomAD_exomes_POPMAX_AF > 0.001 | gnomAD_genomes_POPMAX_AF > 0.001)
 gnomAD_MAF_0.001 <- gnomAD_MAF_0.001 %>% mutate(Class = "B")
 
 ALFA_AF <- c("ALFA_European_AF", "ALFA_African_AF", "ALFA_Asian_AF", "ALFA_Other_AF", "ALFA_Total_AF")
-ALFA_MAF_0.001 <- stoplost_all %>% filter(if_any(all_of(ALFA_AF), ~ . > 0.001))
+ALFA_MAF_0.001 <- stoploss_all %>% filter(if_any(all_of(ALFA_AF), ~ . > 0.001))
 ALFA_MAF_0.001 <- ALFA_MAF_0.001 %>% mutate(Class = "B")
 
 
 # Training set: HGMD + ALFA data
-train_data <- rbind(stoplost_HGMD, ALFA_MAF_0.001) %>% unique()
+train_data <- rbind(stoploss_HGMD, ALFA_MAF_0.001) %>% unique()
 
 # Testing set: ClinVar + gnomAD data
-test_data <- rbind(stoplost_clinvar, gnomAD_MAF_0.001) %>% unique()
+test_data <- rbind(stoploss_clinvar, gnomAD_MAF_0.001) %>% unique()
 
 # Add columns for each amino acid count
 amino_acid_columns <- c("A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y")
-for (aa in amino_acid_columns) {train_data <- train_data %>% mutate(!!aa := str_count(TailAA_seq, aa))}
-for (aa in amino_acid_columns) {test_data <- test_data %>% mutate(!!aa := str_count(TailAA_seq, aa))}
-for (aa in amino_acid_columns) {stoplost_all <- stoplost_all %>% mutate(!!aa := str_count(TailAA_seq, aa))}
+for (aa in amino_acid_columns) {train_data <- train_data %>% mutate(!!aa := str_count(Extension_seq, aa))}
+for (aa in amino_acid_columns) {test_data <- test_data %>% mutate(!!aa := str_count(Extension_seq, aa))}
+for (aa in amino_acid_columns) {stoploss_all <- stoploss_all %>% mutate(!!aa := str_count(Extension_seq, aa))}
 
-hydrophobic_aa <- c("A", "C", "I", "L", "M", "F", "W", "V")
-train_data <- train_data %>% mutate(Hydrophobicity = rowSums(select(., all_of(hydrophobic_aa)))/TailAA_counts)
-test_data <- test_data %>% mutate(Hydrophobicity = rowSums(select(., all_of(hydrophobic_aa)))/TailAA_counts)
-stoplost_all <- stoplost_all %>% mutate(Hydrophobicity = rowSums(select(., all_of(hydrophobic_aa)))/TailAA_counts)
+hydrophobic_aa <- c("A", "C", "I", "L", "M", "F", "V")
+train_data <- train_data %>% mutate(Hydrophobicity = rowSums(select(., all_of(hydrophobic_aa)))/Extension_lengths)
+test_data <- test_data %>% mutate(Hydrophobicity = rowSums(select(., all_of(hydrophobic_aa)))/Extension_lengths)
+stoploss_all <- stoploss_all %>% mutate(Hydrophobicity = rowSums(select(., all_of(hydrophobic_aa)))/Extension_lengths)
 
 # Define features and target
 comp_scores <- c("CADD", "DANN", "FATHMM", "EIGEN", "BayesDel_addAF", "BayesDel_noAF", "int_fitCons", "GERP", "phyloP100way", "phastCons100way")
-gene_feature <- c("Gene_GC", "UTR3_length", "UTR3_GC", "TailAA_counts")
+gene_feature <- c("Gene_GC", "UTR3_length", "UTR3_GC", "Extension_lengths")
 
 # Impute missing values with the median (for numeric data)
 for (scores in comp_scores) {
@@ -77,16 +79,16 @@ for (scores in comp_scores) {
   }
 }
 for (scores in comp_scores) {
-  if (is.numeric(stoplost_all[[scores]])) {
-    stoplost_all[[scores]][is.na(stoplost_all[[scores]])] <- median(stoplost_all[[scores]], na.rm = TRUE)
+  if (is.numeric(stoploss_all[[scores]])) {
+    stoploss_all[[scores]][is.na(stoploss_all[[scores]])] <- median(stoploss_all[[scores]], na.rm = TRUE)
   }
 }
 
 # Prepare the dataset
-train_data <- train_data %>% filter(Class %in% c("B", "P"), is.na(TailAA_counts) == FALSE)
-test_data <- test_data %>% filter(Class %in% c("B", "VUS", "P"), is.na(TailAA_counts) == FALSE)
+train_data <- train_data %>% filter(Class %in% c("B", "P"), is.na(Extension_lengths) == FALSE)
+test_data <- test_data %>% filter(Class %in% c("B", "VUS", "P"), is.na(Extension_lengths) == FALSE)
 
 # Write the filtered dataset to a file
 write.table(train_data, "Train_data_input.txt", row.names = FALSE, sep = "\t", quote = FALSE)
 write.table(test_data, "Test_data_input.txt", row.names = FALSE, sep = "\t", quote = FALSE)
-write.table(stoplost_all, "Stoplost_SNV_input.txt", row.names = FALSE, sep = "\t", quote = FALSE)
+write.table(stoploss_all, "stoploss_SNV_input.txt", row.names = FALSE, sep = "\t", quote = FALSE)
