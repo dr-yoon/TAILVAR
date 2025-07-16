cc86a3219fd39584eef0eafb6d31ea7d43f0e1b1
Jihoon G. Yoon <114216377+dr-yoon@users.noreply.github.com>
2025-07-16 16:41:26 +0900

    Update TAILVAR_score.R

--- code/TAILVAR_score.R
+++ code/TAILVAR_score.R
@@ -48,30 +48,48 @@ SNV_data <- SNV_data %>% left_join(SNV_tango, by = c("Var_ID" = "Variant")) %>% 
 Numeric_variables <- c("CADD", "GERP", "phyloP100", "phastCons100", "pLI", "LOEUF", "UTR3_length", "UTR3_GC", "protein_length", "extended_length", "hydro_KD", "hydro_MJ","mRNA_stability", "TANGO", "CANYA")
 amino_acid_columns <- c("A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y")
 
+# Impute missing values with the median (for numeric data)
+impute_median <- function(df, vars) {
+  for (v in vars) {
+    if (v %in% names(df) && is.numeric(df[[v]])) {
+      df[[v]][is.na(df[[v]])] <- median(df[[v]], na.rm = TRUE)
+    }
+  }
+  return(df)
+}
 
-# Prepare the input data for Random Forest model development
-train_data <- train_data %>% 
-  dplyr::select("Class", all_of(comp_scores), all_of(gene_feature), all_of(amino_acid_columns_rename)) %>% 
-  mutate(Class = factor(Class, levels = c("B", "P")))  # Ensure the target variable is a factor with correct levels
+train_data <- impute_median(train_data, Numeric_variables)
+test_data <- impute_median(test_data, Numeric_variables)
+SNV_data <- impute_median(SNV_data, Numeric_variables)
 
-# Set up cross-validation for model training
-set.seed(123)  # Set seed for reproducibility
+# Prepare the dataset
+train_data <- train_data %>% filter(Class %in% c("B/LB", "P/LP")) %>% mutate(Class = recode(Class, "B/LB" = "B_LB", "P/LP" = "P_LP"), Class = factor(Class, levels = c("B_LB", "P_LP")))
+test_data <- test_data %>% filter(Class %in% c("B/LB", "P/LP")) %>% mutate(Class = recode(Class, "B/LB" = "B_LB", "P/LP" = "P_LP")) %>% dplyr::select(-Upload_variation)
+
+Model_variables  <- c("Upload_variation", "Class", all_of(Numeric_variables), all_of(amino_acid_columns), "IDP")
+comp_scores <- c("CADD", "DANN", "Eigen_PC", "BayesDel_noAF", "BayesDel_addAF", "FATHMM_MKL", "fitCons_int", "MutationTaster", "VEST4", "GPN_MSA", "GERP", "phyloP100", "phastCons100")
+
+train_data <- impute_median(train_data, comp_scores)
+test_data <- impute_median(test_data, comp_scores)
+SNV_data <- impute_median(SNV_data, comp_scores)
+
+# Random Forest model development
+set.seed(123)
+model_train <- train_data %>% dplyr::select(all_of(Model_variables), -Upload_variation)
 train_control <- trainControl(method = "cv", number = 5, classProbs = TRUE, summaryFunction = twoClassSummary)
-options(digits = 10)
 
 # Define grid of hyper-parameters to tune (mtry, ntree)
-tune_grid <- expand.grid(mtry = c(2, 5, 10, 15, 20))  # Different values of mtry (number of variables considered at each split)
-ntree_values <- c(20, 40, 60, 80, 100)                # Different values of ntree (number of trees in the forest)
-maxnodes_values <- c(5, 10, 20, 30)                      # Values for maxnodes
+tune_grid <- expand.grid(mtry = c(2, 5, 10, 15, 20))
+ntree_values <- c(20, 50, 100, 200, 300, 400)
+maxnodes_values <- c(5, 10, 20, 30)
 
-# Initialize a dataframe to store tuning results
 results <- data.frame()
 
 # Perform grid search to optimize mtry, ntree, and maxnodes
 for (maxnodes in maxnodes_values) {
   for (ntree in ntree_values) {
     # Train the model with current ntree value and the entire tune grid for mtry
-    rf_tune <- train(Class ~ ., data = train_data, method = "rf", 
+    rf_tune <- train(Class ~ ., data = model_train, method = "rf", 
                      trControl = train_control, tuneGrid = tune_grid, metric = "ROC", 
                      ntree = ntree, maxnodes = maxnodes)
     
@@ -88,59 +106,18 @@ for (maxnodes in maxnodes_values) {
   }
 }
 
-# Plot AUROC vs. hyper-parameters (mtry, ntree, maxnodes) to visualize model performance
-svg("Hyperparameters.svg", width = 8, height = 8)
-ggplot(results, aes(x = ntree, y = ROC, color = factor(mtry))) +
-  geom_line(size = 1.2) + geom_point(size = 3) +
-  labs(title = "AUROC vs. Hyper-parameters (ntree, mtry) Faceted by maxnodes",
-       x = "Number of Trees (ntree)", y = "AUROC", color = "mtry") +
-  facet_wrap(~ maxnodes) +  # Add faceting by maxnodes
-  theme_minimal() + theme(legend.position = "right")
-dev.off()
-
 # Select the best AUROC combination of mtry, ntree, maxnodes
-selected_params <- c(10, 100, 30)
+selected_params <- c(20, 100, 30)
 cat("Selected mtry:", selected_params[1], "ntree:", selected_params[2], "maxnode:", selected_params[3],"\n")
 
 # Train the final Random Forest model using the selected hyper-parameters
-final_rf_model <- randomForest(Class ~ ., data = train_data, 
-                               mtry = selected_params[1], ntree = selected_params[2], maxnodes = selected_params[3])
+final_rf_model <- randomForest(Class ~ ., data = model_train, mtry = selected_params[1], ntree = selected_params[2], maxnodes = selected_params[3])
 
 # Predict TAILVAR scores for all datasets using the final model
-train_data$TAILVAR <- predict(final_rf_model, train_data, type = "prob")[, "P"]
-test_data$TAILVAR <- predict(final_rf_model, test_data, type = "prob")[, "P"]
-stoploss_all$TAILVAR <- predict(final_rf_model, stoploss_all, type = "prob")[, "P"]
-stoploss_all_vcf <- stoploss_all %>% select(Chromosome, Position, REF_ALLELE, Allele, TAILVAR)
+train_data$TAILVAR <- predict(final_rf_model, train_data, type = "prob")[, "P_LP"]
+test_data$TAILVAR <- predict(final_rf_model, test_data, type = "prob")[, "P_LP"]
+SNV_data$TAILVAR <- predict(final_rf_model, SNV_data, type = "prob")[, "P_LP"]
 
-# Save the TAILVAR scores to output files
 write.table(train_data, "Train_TAILVAR_score.txt", row.names = FALSE, sep = "\t", quote = FALSE)
 write.table(test_data, "Test_TAILVAR_score.txt", row.names = FALSE, sep = "\t", quote = FALSE)
-write.table(stoploss_all, "stoploss_SNV_TAILVAR_score.txt", row.names = FALSE, sep = "\t", quote = FALSE)
-write.table(stoploss_all_vcf, "TAILVAR_score_vcf_input.txt", col.names = FALSE, row.names = FALSE, sep = "\t", quote = FALSE)
-
-# Calculate the correlation matrix for the selected parameters
-correlation_matrix <- cor(train_data %>% dplyr::select(all_of(comp_scores),all_of(gene_feature),all_of(gene_feature),"H_index"), method = "spearman", use = "complete.obs")
-print(correlation_matrix)
-correlation_melt <- melt(correlation_matrix)
-
-# Plot the Spearman rank correlation matrix using corrplot
-col <- colorRampPalette(c("#4477AA","#77AADD", "#FFFFFF", "#EE9988", "#BB4444"))
-svg("Correlation_plot.svg", width = 8, height = 8)
-corrplot(correlation_matrix, method = "circle", type = "upper", col = col(100), 
-         tl.col = "black", tl.srt = 45, addCoef.col = "black", number.cex = 0.7, mar = c(0, 0, 1, 0))
-dev.off()
-
-# Extract variable importance using Gini index
-variable_importance <- importance(final_rf_model, type = 2)
-importance_df <- data.frame(Variable = rownames(variable_importance), Importance = variable_importance[, "MeanDecreaseGini"])
-importance_df <- importance_df %>% mutate(RelativeImportance = Importance / sum(Importance) * 100) %>%
-  arrange(desc(RelativeImportance)) %>%  slice_head(n = 20)
-
-# Plot the normalized importance scores
-svg("Feature_Importance.svg", width = 9, height = 6)
-importance_df %>% arrange(desc(RelativeImportance)) %>%
-  ggplot(aes(x = reorder(Variable, RelativeImportance, decreasing = TRUE), y = RelativeImportance)) +
-  geom_bar(stat = "identity", fill = "#228B22") + geom_text(aes(label = round(RelativeImportance, 1)), vjust = 1.5, size = 3.5, color = "black") +
-  labs(x = "", y = "Relative importance (%)") +
-  theme_classic() + theme(axis.title.y = element_text(size = 18), axis.text.x = element_text(size = 14, angle = -45, hjust = 0)) + scale_y_break(c(10, 30))
-dev.off()
+write.table(SNV_data, "stoploss_SNV_TAILVAR_score.txt", row.names = FALSE, sep = "\t", quote = FALSE)
